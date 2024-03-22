a1bbd12e233e05c3c5159adc57efabe6867d77cb
jas0nle <62453683+jas0nle@users.noreply.github.com>
2024-03-22 00:43:19 -0400

    updating footer

--- components/cta.js
+++ components/cta.js
@@ -10,7 +10,6 @@ const Cta = () => {
   };
   return (
     <Container>
-      <py-script src="main.py" />
       <div className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-indigo-600 px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl">
         <div className="flex-grow text-center lg:text-left">
           <h2 className="text-2xl font-medium lg:text-3xl">
