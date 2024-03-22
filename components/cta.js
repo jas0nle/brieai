0cee3e66392c49875271fb731b4794e8ed7f9e2d
jas0nle <62453683+jas0nle@users.noreply.github.com>
2024-03-22 00:53:44 -0400

    updating footer

--- components/cta.js
+++ components/cta.js
@@ -35,15 +35,18 @@ const Cta = () => {
                 onChange={handleFileUpload}
               />
             </label>
-            <html>
-              <button
-                className="inline-block py-3 mx-auto text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-10 lg:py-5 cursor-pointer"
-                py-onClick="on_click"
-              >
-                Submit
-              </button>
-              <py-script>def on_click(): console.log("Hello world2")</py-script>
-            </html>
+            <button
+              className="inline-block py-3 mx-auto text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-10 lg:py-5 cursor-pointer"
+              py-onClick="on_click"
+            >
+              Submit
+            </button>
+            <py-script>
+              from js import document from pyodide import create_proxy def
+              runPython(): print("you clicked me") function_proxy =
+              create_proxy(runPython)
+              document.getElementById("button").addEventListener("click",function_proxy)
+            </py-script>
           </form>
         </div>
       </div>
