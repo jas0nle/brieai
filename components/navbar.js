ee846729e90975a4d61d54947b4e18d57b16d0d6
jas0nle <62453683+jas0nle@users.noreply.github.com>
2024-03-21 23:10:24 -0400

    updating navbar

--- components/navbar.js
+++ components/navbar.js
@@ -5,7 +5,7 @@ import { Disclosure } from "@headlessui/react";
 
 const Navbar = () => {
   const navigation = [
-    <Link href="/product" className="brie.html">
+    <Link href="/brie.html" className="brie.html">
       Product
     </Link>,
     "Features",
