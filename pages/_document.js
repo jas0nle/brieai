6135702620855ef56d41d2ccb35e630dc8f066f1
jas0nle <62453683+jas0nle@users.noreply.github.com>
2024-03-22 00:14:25 -0400

    updating navbar

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,14 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="stylesheet"
+            href="https://pyscript.net/releases/2024.1.1/core.css"
+          />
+          <script
+            type="module"
+            src="https://pyscript.net/releases/2024.1.1/core.js"
+          ></script>
         </Head>
         <body>
           <Main />
