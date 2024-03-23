416b283598c88da06a343f4ad55ec48603b016a9
jas0nle <62453683+jas0nle@users.noreply.github.com>
2024-03-23 19:41:24 -0400

    updating cta

--- components/main.py
+++ components/main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/home",methods=['POST','GET'])
 
 def upload_video():
+    if request.method == "POST":
+        print(request.files)
+        image = request.files['file']
     return render_template("second.js")
 
 app.run()
